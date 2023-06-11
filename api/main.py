1cabbc7136e5d2d1898ddb401408b32557d63d0d
BalajiSai2003 <balajisai2003@outlook.com>
2023-06-11 20:49:02 +0530

    main.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,23 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+from ..steganography.steganography import encode_message, decode_message
 
 app = FastAPI()
 
+class Message(BaseModel):
+    text: str
+    password: str
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/encode")
+def encode(message: Message):
+    image_path = "path/to/image.png"  # Provide the path to your image file
+    encoded_image_path = "path/to/encoded_image.png"  # Provide the path to save the encoded image
+    encoded_image = encode_message(message.text, message.password, image_path)
+    encoded_image.save(encoded_image_path)
+    return {"message": "Message encoded successfully"}
+
+@app.post("/decode")
+def decode(password: str):
+    encoded_image_path = "path/to/encoded_image.png"  # Provide the path to your encoded image file
+    decoded_message = decode_message(password, encoded_image_path)
+    return {"decoded_message": decoded_message}
