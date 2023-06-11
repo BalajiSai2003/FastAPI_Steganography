238ffe6fff5b35f8accbe7b9470af36aa7df0677
BalajiSai2003 <balajisai2003@outlook.com>
2023-06-11 22:31:57 +0530

    changes  and new steg algo

--- api/models.py
+++ api/models.py
@@ -2,4 +2,3 @@ from pydantic import BaseModel
 
 class Message(BaseModel):
     text: str
-    password: str
--- api/steganography.py
+++ api/steganography.py
@@ -1,57 +1,84 @@
-import cv2
-import numpy as np
-
-def encode_message(message, password, image_path):
-    # Read the image
-    image = cv2.imread(image_path)
-
-    # Check if the image is valid
-    if image is None:
-        raise ValueError("Invalid image file")
-
-    # Convert the message to binary representation
-    binary_message = ''.join(format(ord(char), '08b') for char in message)
-
-    # Generate the encryption key based on the password
-    np.random.seed(sum(ord(char) for char in password))
-    key = np.random.randint(0, 256, size=len(binary_message))
-
-    # Encode the binary message into the image pixels
-    encoded_image = np.copy(image)
-    for i, bit in enumerate(binary_message):
-        row = i // image.shape[1]
-        col = i % image.shape[1]
-        if bit == '1':
-            encoded_image[row, col] = np.bitwise_or(image[row, col], 1)
+from PIL import Image
+import os
+import numpy as np 
+def encode_message(image_path, message):
+    """
+    Encodes a message into an image using the LSB technique.
+    """
+    # Open image and convert to RGB format
+    img = Image.open(os.path.join(image_path)).convert('RGB')
+    # Convert image to numpy array
+    img_array = np.array(img)
+    # Get image dimensions
+    height, width, channels = img_array.shape
+    # Convert message to binary
+    message_bin = ''.join(format(ord(c), '08b') for c in message)
+    # Add end of message delimiter
+    message_bin += '1111111111111110'
+    # Get the maximum number of bits to encode
+    max_bits = height * width * channels // 8
+    if len(message_bin) > max_bits:
+        raise ValueError('Message is too large to encode in image.')
+    # Convert message length to binary and pad with zeros
+    message_length = format(len(message_bin), '016b')
+    # Add message length to the beginning of the binary message
+    message_bin = message_length + message_bin
+    # Iterate over each pixel in the image
+    index = 0
+    for row in range(height):
+        for col in range(width):
+            for channel in range(channels):
+                pixel_value = img_array[row, col, channel]
+                if index < len(message_bin):
+                    # Get the least significant bit of the pixel value
+                    pixel_bin = format(pixel_value, '08b')[:-1] + message_bin[index]
+                    # Convert new pixel value back to integer
+                    new_pixel_value = int(pixel_bin, 2)
+                    # Modify pixel value
+                    img_array[row, col, channel] = new_pixel_value
+                    index += 1
+                else:
+                    break
+            else:
+                continue
+            break
         else:
-            encoded_image[row, col] = np.bitwise_and(image[row, col], 254)
+            continue
+        break
+    # Convert numpy array back to image
+    img_encoded = Image.fromarray(img_array)
+    # Save the encoded image
+    img_encoded.save('../encodedimage/encoded.png')
+    return {"message": "Message encoded successfully"}
 
-    # Apply the encryption key to the encoded image
-    encoded_image ^= key.reshape(image.shape)
 
-    return encoded_image
-
-
-def decode_message(password, image_path):  
-    # sourcery skip: inline-immediately-returned-variable
-    # Read the encoded image
-    encoded_image = cv2.imread(image_path)
-
-    # Check if the image is valid
-    if encoded_image is None:
-        raise ValueError("Invalid image file")
-
-    # Generate the decryption key based on the password
-    np.random.seed(sum(ord(char) for char in password))
-    key = np.random.randint(0, 256, size=encoded_image.size)
-
-    # Apply the decryption key to the encoded image
-    decoded_image = encoded_image ^ key.reshape(encoded_image.shape)
-
-    binary_message = ''.join(
-        '1' if pixel % 2 == 1 else '0' for pixel in decoded_image.reshape(-1)
-    )
-    # Convert the binary message to text
-    decoded_message = ''.join(chr(int(binary_message[i:i+8], 2)) for i in range(0, len(binary_message), 8))
-
-    return decoded_message
+def decode_message(image_path):
+    """
+    Decodes a message from an image using the LSB technique.
+    """
+    # Open image and convert to RGB format
+    img = Image.open(os.path.join(image_path)).convert('RGB')
+    # Convert image to numpy array
+    img_array = np.array(img)
+    # Get image dimensions
+    height, width, channels = img_array.shape
+    # Iterate over each pixel in the image
+    message_bin = ''
+    for row in range(height):
+        for col in range(width):
+            for channel in range(channels):
+                pixel_value = img_array[row, col, channel]
+                # Get the least significant bit of the pixel value
+                pixel_bin = format(pixel_value, '08b')[-1]
+                # Append least significant bit to message binary string
+                message_bin += pixel_bin
+                # Check if message delimiter is found
+                if message_bin[-16:] == '1111111111111110':
+                    # Extract message length from binary string
+                    message_length = int(message_bin[:16], 2)
+                    # Extract message from binary string
+                    message_bin = message_bin[16:message_length+16]
+                    # Convert binary message back to text
+                    message = ''.join(chr(int(message_bin[i:i+8], 2)) for i in range(0, len(message_bin), 8))
+                    return message
+            
